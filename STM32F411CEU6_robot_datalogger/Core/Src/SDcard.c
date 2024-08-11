bc23ff1ebb2fc155b84a635102b25bd90aa78a1f
Pedro Hugo <pedro.hugo@ufba.br>
2024-08-11 20:01:59 -0300

    Insert comments and functions on freertos module

--- STM32F411CEU6_robot_datalogger/Core/Src/SDcard.c
+++ STM32F411CEU6_robot_datalogger/Core/Src/SDcard.c
@@ -30,179 +30,249 @@ QueueHandle_t SDcard_get_queue_handle(void){
 	return SDcard_queue_handle;
 }
 
-static bool path_select(char* path, size_t buff_size, data_type_t data_type){
-	if(buff_size < 50*sizeof(char)){
-		USB_PRINT("path buffer size too small\n");
-		return false;
-	}
-	*path = '\0';
-	switch(data_type){
-				case CORRENTE:
-					strcpy(path, "CORRENTE.csv");
-					break;
-				case VEL_ANG_MOTOR:
-					strcpy(path, "VEL_ANG_MOTOR.csv");
-					break;
-				case ACEL_LIN:
-					strcpy(path, "ACEL_LIN.csv");
-					break;
-				case VEL_ANG_GIRO:
-					strcpy(path, "VEL_ANG_GIRO.csv");
-					break;
-				case CAMPO_MAG:
-					strcpy(path, "CAMPO_MAG.csv");
-					break;
-				case A_CTRL_TRACAO:
-					strcpy(path, "A_CTRL_TRACAO.csv");
-					break;
-				case G_CTRL_TRACAO:
-					strcpy(path, "G_CTRL_TRACAO.csv");
-					break;
-				case G_CTRL_TRACAO_RX:
-					strcpy(path, "G_CTRL_TRACAO_RX.csv");
-					break;
-				case A_CTRL_VEL:
-					strcpy(path, "A_CTRL_VEL.csv");
-					break;
-				case S_POINT_VEL:
-					strcpy(path, "S_POINT_VEL.csv");
-					break;
-				case G_CTRL_VEL:
-					strcpy(path, "G_CTRL_VEL.csv");
-					break;
-				case G_CTRL_VEL_RX:
-					strcpy(path, "G_CTRL_VEL_RX.csv");
-					break;
-				case ANG_ROT_BASE:
-					strcpy(path, "ANG_ROT_BASE.csv");
-					break;
-				case GPS:
-					strcpy(path, "GPS.csv");
-					break;
-				case A_CTRL_POSI:
-					strcpy(path, "A_CTRL_POSI.csv");
-					break;
-				case G_CTRL_POSI:
-					strcpy(path, "G_CTRL_POSI.csv");
-					break;
-				case G_CTRL_POSI_RX:
-					strcpy(path, "G_CTRL_POSI_RX.csv");
-					break;
-				default:
-					USB_PRINT("type not supported\n");
-					return false;
-			}
-	return true;
+/**
+ * Seleciona o caminho do arquivo correspondente a um tipo de dado específico.
+ *
+ * A função `path_select` verifica se o buffer de caminho fornecido é grande o suficiente e, em seguida,
+ * copia o nome do arquivo apropriado, baseado no tipo de dado (`data_type`), para o buffer `path`.
+ * Cada tipo de dado está associado a um arquivo CSV específico.
+ *
+ * @param path Ponteiro para o buffer de string onde o caminho do arquivo será armazenado.
+ * @param buff_size Tamanho do buffer de caminho em bytes.
+ * @param data_type Tipo de dado para o qual o caminho do arquivo deve ser selecionado.
+ * @return `true` se o caminho foi selecionado com sucesso, `false` caso contrário.
+ */
+static bool path_select(char* path, size_t buff_size, data_type_t data_type) {
+    // Verifica se o tamanho do buffer é grande o suficiente para armazenar o nome do arquivo.
+    if(buff_size < 50 * sizeof(char)) {
+        USB_PRINT("path buffer size too small\n");
+        return false;  // Retorna falso se o buffer for pequeno demais.
+    }
+
+    *path = '\0';  // Inicializa o buffer de caminho com uma string vazia.
+
+    // Seleciona o caminho do arquivo com base no tipo de dado fornecido.
+    switch(data_type) {
+        case CORRENTE:
+            strcpy(path, "CORRENTE.csv");
+            break;
+        case VEL_ANG_MOTOR:
+            strcpy(path, "VEL_ANG_MOTOR.csv");
+            break;
+        case ACEL_LIN:
+            strcpy(path, "ACEL_LIN.csv");
+            break;
+        case VEL_ANG_GIRO:
+            strcpy(path, "VEL_ANG_GIRO.csv");
+            break;
+        case CAMPO_MAG:
+            strcpy(path, "CAMPO_MAG.csv");
+            break;
+        case A_CTRL_TRACAO:
+            strcpy(path, "A_CTRL_TRACAO.csv");
+            break;
+        case G_CTRL_TRACAO:
+            strcpy(path, "G_CTRL_TRACAO.csv");
+            break;
+        case G_CTRL_TRACAO_RX:
+            strcpy(path, "G_CTRL_TRACAO_RX.csv");
+            break;
+        case A_CTRL_VEL:
+            strcpy(path, "A_CTRL_VEL.csv");
+            break;
+        case S_POINT_VEL:
+            strcpy(path, "S_POINT_VEL.csv");
+            break;
+        case G_CTRL_VEL:
+            strcpy(path, "G_CTRL_VEL.csv");
+            break;
+        case G_CTRL_VEL_RX:
+            strcpy(path, "G_CTRL_VEL_RX.csv");
+            break;
+        case ANG_ROT_BASE:
+            strcpy(path, "ANG_ROT_BASE.csv");
+            break;
+        case GPS:
+            strcpy(path, "GPS.csv");
+            break;
+        case A_CTRL_POSI:
+            strcpy(path, "A_CTRL_POSI.csv");
+            break;
+        case G_CTRL_POSI:
+            strcpy(path, "G_CTRL_POSI.csv");
+            break;
+        case G_CTRL_POSI_RX:
+            strcpy(path, "G_CTRL_POSI_RX.csv");
+            break;
+        default:
+            USB_PRINT("type not supported\n");
+            return false;  // Retorna falso se o tipo de dado não for suportado.
+    }
+
+    return true;  // Retorna verdadeiro se o caminho foi selecionado com sucesso.
 }
 
-static bool reset_files(void){
-	int er = 1;
-	char path[50];
-	char buffer[100];
+/**
+ * Reseta (deleta) arquivos associados a diferentes tipos de dados, se existirem.
+ *
+ * A função `reset_files` percorre uma lista de tipos de dados e tenta deletar os arquivos
+ * correspondentes, caso eles existam. Se algum arquivo não puder ser deletado ou se ocorrer
+ * algum erro durante o processo, a função retorna `false`.
+ *
+ * @return `true` se todos os arquivos foram deletados com sucesso ou se eles não existiam;
+ *         `false` se houve algum erro na seleção do caminho ou durante a deleção dos arquivos.
+ */
+static bool reset_files(void) {
+    int er = 1;                // Variável de erro inicializada com um valor não nulo.
+    char path[50];             // Buffer para armazenar o caminho do arquivo.
+    char buffer[100];          // Buffer para mensagens de erro.
 
-	for(int i = 0; i < NumberOfTypes; i++){
-		er = path_select(path, sizeof(path), i);
-		if(!er){
-			USB_PRINT("error selecting path\n");
-			return false;
-		}
+    // Itera sobre todos os tipos de dados definidos.
+    for(int i = 0; i < NumberOfTypes; i++) {
+        // Seleciona o caminho do arquivo correspondente ao tipo de dado.
+        er = path_select(path, sizeof(path), i);
+        if(!er) {
+            USB_PRINT("error selecting path\n");
+            return false;  // Retorna falso se a seleção do caminho falhar.
+        }
 
-		er = f_stat(path, NULL);
-		if(er != FR_OK){
-			if(er == FR_NO_FILE) continue;
-			sprintf(buffer, "error: %d\n", er);
-			USB_PRINT(buffer);
-			return false;
-		}
+        // Verifica se o arquivo existe utilizando f_stat.
+        er = f_stat(path, NULL);
+        if(er != FR_OK) {
+            if(er == FR_NO_FILE) continue;  // Se o arquivo não existe, continua para o próximo tipo de dado.
+            sprintf(buffer, "error: %d\n", er);
+            USB_PRINT(buffer);  // Imprime a mensagem de erro correspondente.
+            return false;  // Retorna falso se houver algum outro erro.
+        }
 
-		er = f_unlink(path);
-		if(er != FR_OK){
-			sprintf(buffer, "error: %d\n", er);
-			USB_PRINT(buffer);
-			return false;
-		}
-	}
-	return true;
+        // Deleta o arquivo existente.
+        er = f_unlink(path);
+        if(er != FR_OK) {
+            sprintf(buffer, "error: %d\n", er);
+            USB_PRINT(buffer);  // Imprime a mensagem de erro correspondente.
+            return false;  // Retorna falso se a deleção do arquivo falhar.
+        }
+    }
+
+    return true;  // Retorna verdadeiro se todos os arquivos foram deletados com sucesso ou se não existiam.
 }
 
-static void SDcard_task(void* arg){
-	SD_data_t data = {0};
-	char buffer[100];
-	char path[50];
-	int er = 0;
-	for(;;){
-		if(xQueueReceive(SDcard_queue_handle, &data, portMAX_DELAY) != pdTRUE){
-			USB_PRINT("error receive from queue\n");
-			continue;
-		}
+/**
+ * Tarefa responsável por gravar dados na SD card em arquivos CSV.
+ *
+ * A função `SDcard_task` é uma tarefa FreeRTOS que fica em loop infinito, aguardando
+ * dados de uma fila (`SDcard_queue_handle`). Quando recebe os dados, ela grava as
+ * informações correspondentes em um arquivo CSV na SD card. Se o arquivo não existir,
+ * ele é criado e um cabeçalho apropriado é escrito.
+ *
+ * @param arg Parâmetro passado para a tarefa (não utilizado neste contexto).
+ */
+static void SDcard_task(void* arg) {
+    SD_data_t data = {0};  // Estrutura para armazenar os dados recebidos da fila.
+    char buffer[100];      // Buffer para leitura de linhas do arquivo.
+    char path[50];         // Buffer para armazenar o caminho do arquivo.
+    int er = 0;            // Variável para armazenar códigos de erro.
 
-		er = path_select(path, sizeof(path), data.data_type);
-		if(!er){
-			USB_PRINT("error selecting path\n");
-			continue;
-		}
+    for(;;) {
+        // Aguarda até que dados sejam recebidos da fila SDcard_queue_handle.
+        if(xQueueReceive(SDcard_queue_handle, &data, portMAX_DELAY) != pdTRUE) {
+            USB_PRINT("error receive from queue\n");
+            continue;  // Se ocorrer erro ao receber da fila, imprime mensagem e continua.
+        }
 
-		er = f_stat(path, NULL);
-		if(er == FR_NO_FILE){
-			f_open(&fil, path, FA_CREATE_ALWAYS | FA_WRITE);
-			f_printf(&fil, "%s\n", path);
-			if(data.array_size == 1){
-				f_puts("timestamp,value\n", &fil);
-			}
-			else if(data.array_size == 3){
-				f_puts("timestamp,value1,value2,value3\n", &fil);
-			}
-			else{
-				USB_PRINT("incompatible array size\n");
-				continue;
-			}
-			f_close(&fil);
-		}
+        // Seleciona o caminho do arquivo com base no tipo de dado.
+        er = path_select(path, sizeof(path), data.data_type);
+        if(!er) {
+            USB_PRINT("error selecting path\n");
+            continue;  // Se a seleção do caminho falhar, imprime mensagem e continua.
+        }
 
-		er = f_open(&fil, path, FA_OPEN_APPEND | FA_WRITE | FA_READ);
-		if(er != FR_OK){
-			sprintf(buffer, "error: %d\n", er);
-			USB_PRINT(buffer);
-			continue;
-		}
+        // Verifica se o arquivo existe.
+        er = f_stat(path, NULL);
+        if(er == FR_NO_FILE) {
+            // Se o arquivo não existir, cria um novo arquivo e adiciona um cabeçalho.
+            f_open(&fil, path, FA_CREATE_ALWAYS | FA_WRITE);
+            f_printf(&fil, "%s\n", path);  // Escreve o nome do arquivo no início.
 
-		if(data.array_size == 1){
-			f_printf(&fil, "%u,%d\n", data.timestamp, data.value[0]);
-		}
-		else if(data.array_size == 3){
-			f_printf(&fil, "%u,%d,%d,%d\n", data.timestamp, data.value[0], data.value[1], data.value[2]);
-		}
-		else {
-			USB_PRINT("incompatible array size\n");
-			continue;
-		}
+            // Adiciona o cabeçalho com base no tamanho do array de valores.
+            if(data.array_size == 1) {
+                f_puts("timestamp,value\n", &fil);
+            } else if(data.array_size == 3) {
+                f_puts("timestamp,value1,value2,value3\n", &fil);
+            } else {
+                USB_PRINT("incompatible array size\n");
+                f_close(&fil);  // Fecha o arquivo antes de continuar.
+                continue;  // Se o tamanho do array for incompatível, continua.
+            }
 
+            f_close(&fil);  // Fecha o arquivo após adicionar o cabeçalho.
+        }
 
-		f_lseek(&fil, 0);
-		while(f_gets(buffer, sizeof(buffer), &fil))
-		{
-			USB_PRINT(buffer);
-			memset(buffer,0,sizeof(buffer));
-		}
-		USB_PRINT("\n");
-		f_close(&fil);
+        // Abre o arquivo em modo de adição e leitura.
+        er = f_open(&fil, path, FA_OPEN_APPEND | FA_WRITE | FA_READ);
+        if(er != FR_OK) {
+            sprintf(buffer, "error: %d\n", er);
+            USB_PRINT(buffer);  // Imprime o código de erro se a abertura do arquivo falhar.
+            continue;
+        }
 
-	}
+        // Escreve os dados no arquivo, dependendo do tamanho do array.
+        if(data.array_size == 1) {
+            f_printf(&fil, "%u,%d\n", data.timestamp, data.value[0]);
+        } else if(data.array_size == 3) {
+            f_printf(&fil, "%u,%d,%d,%d\n", data.timestamp, data.value[0], data.value[1], data.value[2]);
+        } else {
+            USB_PRINT("incompatible array size\n");
+            f_close(&fil);  // Fecha o arquivo antes de continuar.
+            continue;
+        }
+
+        // Reposiciona o ponteiro do arquivo no início e lê o conteúdo para imprimir via USB.
+        f_lseek(&fil, 0);
+        while(f_gets(buffer, sizeof(buffer), &fil)) {
+            USB_PRINT(buffer);  // Imprime cada linha do arquivo.
+            memset(buffer, 0, sizeof(buffer));  // Limpa o buffer para a próxima leitura.
+        }
+        USB_PRINT("\n");
+        f_close(&fil);  // Fecha o arquivo após o processamento.
+
+    }
 }
 
-int SDcard_start(void){
-	print_smpr_handle = xSemaphoreCreateBinary();
-	SDcard_queue_handle = xQueueCreate(20, sizeof(SD_data_t));
-	if(f_mount(&fs, "", 0) != FR_OK){
-		USB_PRINT("fail mount sd card\n");
-		return false;
-	}
-	if(reset_files() != true){
-		USB_PRINT("fail reseting files\n");
-		return false;
-	}
-	if(xTaskCreate(SDcard_task, "SDcard_task", 128*5, NULL, 26, NULL) != pdPASS) return false;
-	return true;
-}
+/**
+ * Inicializa o sistema de armazenamento em SD card e inicia a tarefa de gerenciamento.
+ *
+ * A função `SDcard_start` realiza a configuração inicial necessária para o funcionamento
+ * do sistema de armazenamento em SD card. Ela cria um semáforo e uma fila para comunicação
+ * entre tarefas, monta o sistema de arquivos, reseta arquivos antigos, e inicia a tarefa
+ * responsável por gravar dados na SD card.
+ *
+ * @return `true` se a inicialização foi bem-sucedida; `false` caso contrário.
+ */
+int SDcard_start(void) {
+    // Cria um semáforo binário para sincronização (por exemplo, para impressão ou outras operações críticas).
+    print_smpr_handle = xSemaphoreCreateBinary();
 
+    // Cria uma fila para armazenar até 20 elementos do tipo SD_data_t, usada para comunicação entre tarefas.
+    SDcard_queue_handle = xQueueCreate(20, sizeof(SD_data_t));
+
+    // Monta o sistema de arquivos na SD card.
+    if(f_mount(&fs, "", 0) != FR_OK) {
+        USB_PRINT("fail mount sd card\n");
+        return false;  // Retorna falso se o sistema de arquivos não puder ser montado.
+    }
+
+    // Reseta (deleta) arquivos antigos se existirem.
+    if(reset_files() != true) {
+        USB_PRINT("fail reseting files\n");
+        return false;  // Retorna falso se ocorrer um erro ao resetar os arquivos.
+    }
+
+    // Cria e inicia a tarefa de gerenciamento da SD card.
+    if(xTaskCreate(SDcard_task, "SDcard_task", 128*5, NULL, 26, NULL) != pdPASS) {
+        return false;  // Retorna falso se a tarefa não puder ser criada.
+    }
+
+    return true;  // Retorna verdadeiro se tudo for inicializado com sucesso.
+}
 
--- STM32F411CEU6_robot_datalogger/Core/Src/freertos.c
+++ STM32F411CEU6_robot_datalogger/Core/Src/freertos.c
@@ -6,7 +6,7 @@
   ******************************************************************************
   * @attention
   *
-  * Copyright (c) 2024 STMicroelectronics.
+  * Copyright (c) 2022 STMicroelectronics.
   * All rights reserved.
   *
   * This software is licensed under terms that can be found in the LICENSE file
@@ -25,7 +25,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "SDcard.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -45,19 +45,27 @@
 
 /* Private variables ---------------------------------------------------------*/
 /* USER CODE BEGIN Variables */
+static const data_type_t one_1[] = {CORRENTE, A_CTRL_TRACAO};
+static const data_type_t one_10[] = {VEL_ANG_MOTOR, A_CTRL_VEL, S_POINT_VEL};
+static const data_type_t one_100[] = {A_CTRL_POSI};
+static const data_type_t three_1[] = {G_CTRL_TRACAO};
+static const data_type_t three_10[] = {ACEL_LIN, VEL_ANG_GIRO, CAMPO_MAG, G_CTRL_VEL, ANG_ROT_BASE};
+static const data_type_t three_100[] = {GPS, G_CTRL_POSI};
+static const data_type_t three_ap[] = {G_CTRL_TRACAO_RX, G_CTRL_VEL_RX, G_CTRL_POSI_RX};
 
 /* USER CODE END Variables */
 /* Definitions for defaultTask */
 osThreadId_t defaultTaskHandle;
 const osThreadAttr_t defaultTask_attributes = {
   .name = "defaultTask",
-  .stack_size = 128 * 4,
+  .stack_size = 128 * 8,
   .priority = (osPriority_t) osPriorityNormal,
 };
 
 /* Private function prototypes -----------------------------------------------*/
 /* USER CODE BEGIN FunctionPrototypes */
-
+void send_data(void* arg);
+static bool start_send_data_tasks(void);
 /* USER CODE END FunctionPrototypes */
 
 void StartDefaultTask(void *argument);
@@ -117,16 +125,181 @@ void StartDefaultTask(void *argument)
   /* init code for USB_DEVICE */
   MX_USB_DEVICE_Init();
   /* USER CODE BEGIN StartDefaultTask */
+
+  vTaskDelay(3000);
+  if(!SDcard_start()){
+	  USB_PRINT("error starting sd card\n");
+	  return;
+  }
+  if(!start_send_data_tasks()){
+	  USB_PRINT("error starting send data tasks\n");
+  }
+
+
   /* Infinite loop */
   for(;;)
   {
-    osDelay(1);
+    vTaskDelay(1000);
   }
   /* USER CODE END StartDefaultTask */
 }
 
 /* Private application code --------------------------------------------------*/
 /* USER CODE BEGIN Application */
+
+/**
+ * Função que gera e envia dados periodicamente para a fila da tarefa SD card.
+ *
+ * A função `send_data` é uma tarefa FreeRTOS que gera dados simulados com base em
+ * configurações fornecidas como argumento. Os dados gerados são enviados para a fila
+ * que a tarefa de gerenciamento da SD card utiliza para gravar esses dados no cartão.
+ * A função executa um número limitado de iterações e, em seguida, exclui a tarefa.
+ *
+ * @param arg Parâmetro de entrada, convertido para um valor de 32 bits, que contém as configurações
+ * de período, tipo de dado, e tamanho do array.
+ */
+void send_data(void* arg) {
+    uint32_t settings_in_bits = (uint32_t)arg; // Configurações passadas como argumento
+    QueueHandle_t SDcard_queue = SDcard_get_queue_handle(); // Obtém o handle da fila SD card
+    uint16_t period_ms = 0;  // Período de envio em milissegundos
+    uint8_t array_size = 0;  // Tamanho do array de valores (1 ou 3)
+    data_type_t type = 0;    // Tipo de dado a ser enviado
+    char buf[35];            // Buffer para mensagens de erro
+    int n = 0;               // Contador de iterações
+
+    // Determina o tamanho do array com base no bit mais significativo (31º bit) de settings_in_bits
+    if (settings_in_bits >> 31) {
+        array_size = 3;
+    } else {
+        array_size = 1;
+    }
+
+    // Extrai o período em milissegundos dos bits 16 a 30 de settings_in_bits
+    period_ms = (settings_in_bits << 1) >> 16;
+
+    // Extrai o tipo de dado dos bits 0 a 15 de settings_in_bits
+    type = (settings_in_bits << 16) >> 16;
+
+    // Inicializa o gerador de números aleatórios com base no tipo de dado
+    srand(type);
+
+    // Armazena o tempo atual para uso em delays periódicos
+    TickType_t LastWakeTime = xTaskGetTickCount();
+
+    for (;;) {
+        if (n == 3) break; // Limita a execução a 3 iterações
+
+        // Atraso periódico baseado no período em milissegundos
+        vTaskDelayUntil(&LastWakeTime, pdMS_TO_TICKS(period_ms));
+
+        // Cria a estrutura de dados a ser enviada
+        SD_data_t data = {
+            .value = {rand() % 100, rand() % 100, rand() % 100}, // Valores aleatórios
+            .data_type = type, // Tipo de dado
+            .timestamp = LastWakeTime, // Timestamp da última execução
+            .array_size = array_size, // Tamanho do array de valores
+        };
+
+        // Envia os dados para a fila da tarefa SD card
+        if (xQueueSend(SDcard_queue, (void*)&data, pdMS_TO_TICKS(period_ms) / 2) != pdTRUE) {
+            // Em caso de falha, imprime uma mensagem de erro
+            sprintf(buf, "fail send data to queue - %d\n", data.data_type);
+            USB_PRINT(buf);
+        }
+
+        n++; // Incrementa o contador de iterações
+    }
+
+    // Exclui a tarefa após 3 iterações
+    vTaskDelete(NULL);
+}
+
+
+/**
+ * Função para iniciar várias tarefas de envio de dados.
+ *
+ * Esta função cria múltiplas tarefas FreeRTOS, cada uma configurada para enviar dados
+ * periodicamente, conforme definido pelos parâmetros fornecidos. As tarefas são configuradas
+ * para enviar dados com diferentes tamanhos de array e períodos.
+ *
+ * @return Retorna `true` se todas as tarefas forem criadas com sucesso, caso contrário, retorna `false`.
+ */
+static bool start_send_data_tasks(void) {
+    BaseType_t ret = pdTRUE;
+    uint32_t settings_in_bits = 0UL;
+
+    // Configuração para tarefas com array de 1 elemento e período de 1 ms
+    settings_in_bits = 0 << 31; // 1 elemento no array
+    settings_in_bits += 1 << 15; // Período de 1 ms
+    for (int i = 0; i < sizeof(one_1)/sizeof(one_1[0]); i++) {
+        settings_in_bits += one_1[i]; // Tipo de dado
+        ret = xTaskCreate(send_data, "send_data", 128*2, (void*)settings_in_bits, 25, NULL);
+        if (ret != pdPASS) return false; // Falha na criação da tarefa
+        settings_in_bits = (settings_in_bits >> 15) << 15; // Reset dos bits menos significativos
+    }
+
+    // Configuração para tarefas com array de 1 elemento e período de 10 ms
+    settings_in_bits = 0 << 31; // 1 elemento no array
+    settings_in_bits += 10 << 15; // Período de 10 ms
+    for (int i = 0; i < sizeof(one_10)/sizeof(one_10[0]); i++) {
+        settings_in_bits += one_10[i]; // Tipo de dado
+        ret = xTaskCreate(send_data, "send_data", 128*2, (void*)settings_in_bits, 25, NULL);
+        if (ret != pdPASS) return false; // Falha na criação da tarefa
+        settings_in_bits = (settings_in_bits >> 15) << 15; // Reset dos bits menos significativos
+    }
+
+    // Configuração para tarefas com array de 1 elemento e período de 100 ms
+    settings_in_bits = 0 << 31; // 1 elemento no array
+    settings_in_bits += 100 << 15; // Período de 100 ms
+    for (int i = 0; i < sizeof(one_100)/sizeof(one_100[0]); i++) {
+        settings_in_bits += one_100[i]; // Tipo de dado
+        ret = xTaskCreate(send_data, "send_data", 128*2, (void*)settings_in_bits, 25, NULL);
+        if (ret != pdPASS) return false; // Falha na criação da tarefa
+        settings_in_bits = (settings_in_bits >> 15) << 15; // Reset dos bits menos significativos
+    }
+
+    // Configuração para tarefas com array de 3 elementos e período de 1 ms
+    settings_in_bits = 1 << 31; // 3 elementos no array
+    settings_in_bits += 1 << 15; // Período de 1 ms
+    for (int i = 0; i < sizeof(three_1)/sizeof(three_1[0]); i++) {
+        settings_in_bits += three_1[i]; // Tipo de dado
+        ret = xTaskCreate(send_data, "send_data", 128*2, (void*)settings_in_bits, 25, NULL);
+        if (ret != pdPASS) return false; // Falha na criação da tarefa
+        settings_in_bits = (settings_in_bits >> 15) << 15; // Reset dos bits menos significativos
+    }
+
+    // Configuração para tarefas com array de 3 elementos e período de 10 ms
+    settings_in_bits = 1 << 31; // 3 elementos no array
+    settings_in_bits += 10 << 15; // Período de 10 ms
+    for (int i = 0; i < sizeof(three_10)/sizeof(three_10[0]); i++) {
+        settings_in_bits += three_10[i]; // Tipo de dado
+        ret = xTaskCreate(send_data, "send_data", 128*2, (void*)settings_in_bits, 25, NULL);
+        if (ret != pdPASS) return false; // Falha na criação da tarefa
+        settings_in_bits = (settings_in_bits >> 15) << 15; // Reset dos bits menos significativos
+    }
+
+    // Configuração para tarefas com array de 3 elementos e período de 100 ms
+    settings_in_bits = 1 << 31; // 3 elementos no array
+    settings_in_bits += 100 << 15; // Período de 100 ms
+    for (int i = 0; i < sizeof(three_100)/sizeof(three_100[0]); i++) {
+        settings_in_bits += three_100[i]; // Tipo de dado
+        ret = xTaskCreate(send_data, "send_data", 128*2, (void*)settings_in_bits, 25, NULL);
+        if (ret != pdPASS) return false; // Falha na criação da tarefa
+        settings_in_bits = (settings_in_bits >> 15) << 15; // Reset dos bits menos significativos
+    }
+
+    // Configuração para tarefas com array de 3 elementos e período aleatório (até 1000 ms)
+    settings_in_bits = 1 << 31; // 3 elementos no array
+    settings_in_bits += rand() % 1000 << 15; // Período aleatório em ms
+    for (int i = 0; i < sizeof(three_ap)/sizeof(three_ap[0]); i++) {
+        settings_in_bits += three_ap[i]; // Tipo de dado
+        ret = xTaskCreate(send_data, "send_data", 128*2, (void*)settings_in_bits, 25, NULL);
+        if (ret != pdPASS) return false; // Falha na criação da tarefa
+        settings_in_bits = (settings_in_bits >> 15) << 15; // Reset dos bits menos significativos
+    }
+
+    return true; // Todas as tarefas foram criadas com sucesso
+}
 
 /* USER CODE END Application */
 
